69777141843ce5478fa38c95b64dc9d329569c24
jack <17389173008@163.com>
2020-06-25 15:33:24 +0800

    modigy to 官方暴力法

--- Week_01/leetcode_189.java
+++ Week_01/leetcode_189.java
@@ -1,12 +1,14 @@
 class Solution {
-    // System.arraycopy()方法，类似于官方的暴力法
+    // 官方暴力法
     public void rotate(int[] nums, int k) {
         int len = nums.length;
-        //int[] temp = new int[len-1];
-        for (int i=0; i<k; i++) {
-            int cur = nums[len-1];
-            System.arraycopy(nums, 0, nums, 1, len-1);
-            nums[0] = cur;
+        for (int id=0; id<k; id++) {
+            int prev = nums[len-1];
+            for (int i=0; i<len; i++) {
+                int temp = nums[i];
+                nums[i] = prev;
+                prev = temp;
+            }
         }
     }
 }
