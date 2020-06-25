109b9ca188632e3ec08182600f83a085da4d2edb
jack <17389173008@163.com>
2020-06-25 19:09:57 +0800

    modify leetcode_189.java use 循环交换法(用交换次数控制)

--- Week_01/leetcode_189.java
+++ Week_01/leetcode_189.java
@@ -1,36 +1,23 @@
 class Solution {
     // 官方题解3：循环交换
     // 需要考虑两种情况：n 与 k的最大公约数==1，一轮完成
-    //                n 与 k的最大公约数==m, m轮才能完成
+    //                 n 与 k的最大公约数==m, m轮才能完成
 
+    // 用交换次数count 控制
     public void rotate(int[] nums, int k) {
-        if (k==0 || nums==null || nums.length==0)
-            return;
-        int N = GCD(k,nums.length); //最大公约数
-        for (int i=0; i<N; i++) {
+        k %= nums.length;
+        int count = 0;
+        for (int i=0; count < nums.length; i++) {
             int cur = i;
             int prev = nums[cur];
             do {
-                int next = (cur+k) % nums.length;
+                int next = (cur + k) % nums.length;
                 int temp = nums[next];
                 nums[next] = prev;
                 prev = temp;
                 cur = next;
+                count++;
             } while (cur != i);
         }
-    }
-
-    // 求最大公约数
-    public int GCD(int m, int n){
-        int temp;
-        // m：大数 n：小数
-        if (m < n) { //
-            temp = n;
-            n = m;
-            m = temp;
-        }
-        if (m % n == 0)
-            return n;
-        return GCD(n,m%n);
     }
 }
