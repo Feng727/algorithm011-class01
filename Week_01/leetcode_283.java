7ded9545fd0c4e3e7eae1f1b1dcde6c54339b73d
jack <17389173008@163.com>
2020-06-26 14:46:58 +0800

    modify leetcode_283

--- Week_01/leetcode_283.java
+++ Week_01/leetcode_283.java
@@ -1,18 +1,14 @@
 class Solution {
     //快慢双指针法
     public void moveZeroes(int[] nums) {
-        // 思路：先将所有非零元素移动到前面，记录0的个数，最后在后边补0
-        int count = 0;
-        int index = 0;
+         // 思路：直接将非0<——>0交换，1次遍历
+        int index_nozero = 0; //记录非0元素的索引
         for (int i=0; i<nums.length; i++) {
             if (nums[i] != 0) {
-                nums[index++] = nums[i];
-            }else{
-                count++;
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[index_nozero++] = temp;
             }
-        }
-        for (int i=index; i<nums.length; i++) {
-            nums[i] = 0;
         }
     }
 }
